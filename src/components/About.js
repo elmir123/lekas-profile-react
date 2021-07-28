a02895997f0656102f8c1f5a349a2d9249dede6d
Elmir Lekovic <elmir123@gmai.com>
2021-07-28 19:28:58 -0400

    CSS of the APP, poroject and about js files

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,34 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      backgroundColor: theme.palette.primary.contrastText,
+      padding: theme.spacing(2),
+      textAlign: 'left',
+      color: theme.palette.text.primary,
+    },
+  }));
 const About = () => {
+    const classes = useStyles();
     return (
-        <div>About
-            <Button variant="contained">Default</Button>
-            <Button variant="contained" color="primary">
-            Primary
-            </Button>
+        <div>
+            <h1 className="pageTitle">About Me</h1>
+            <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        
+        </Grid>
         </div>
     )
 }
