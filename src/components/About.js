19940b6a73d1b264406c01de935c236092950b47
Elmir Lekovic <elmir123@gmai.com>
2021-07-28 20:10:49 -0400

    Adding New Images

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Button from '@material-ui/core/Button';
-
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -25,8 +24,40 @@ const About = () => {
             <h1 className="pageTitle">About Me</h1>
             <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+          <Paper className={classes.paper}>
+              <p><h3>Elmir Lekovic</h3> has 18 years of IT experience. 13 years working experience as a Full Stack Developer using Linux, Django/Python, CSS, HTML, JavaScript. Working Experience with: React, Node.js, WordPress. Self-starting entrepreneur. Demonstrated time management, organizational, multi-tasking, trouble-shooting, and prioritization skills with an emphasis on communication and team work.
+Always open to new experiences and excited for technical challenges.
+            </p>
+</Paper>
+        
         </Grid>
+        <Grid item xs={12}>
+        <div>
+            <Carousel>
+                <div>
+                    <img src="assets/imgs/cns_logo.png" alt="CANADIAN NUCLEAR SOCIETY" />
+                    <p className="legend">CANADIAN NUCLEAR SOCIETY</p>
+                </div>
+                <div>
+                    <img src="assets/imgs/ocea-logo.jpeg" alt="OTTAWA CHILDBIRTH EDUCATION ASSOCIATION" />
+                    <p className="legend">OTTAWA CHILDBIRTH EDUCATION ASSOCIATION</p>
+                </div>
+                <div>
+                    <img src="assets/imgs/summerfrest.png" alt="DEEP RIVER SUMMERFEST" />
+                    <p className="legend">DEEP RIVER SUMMERFEST</p>
+                </div>
+                <div>
+                    <img src="assets/imgs/dawn-new.png" alt="DAWN CANADA" />
+                    <p className="legend">DAWN CANADA</p>
+                </div>
+                <div>
+                    <img src="assets/imgs/cropped-erin-1.png" alt="ERIN SHAHEEN" />
+                    <p className="legend">ERIN SHAHEEN</p>
+                </div>
+            </Carousel>
+        </div>
+        </Grid>
+
         
         </Grid>
         </div>
